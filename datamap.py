ed96744f85f7a62f1a0ab1f986f956729fd3a762
jukahkon <juha.m.kahkonen@gmail.com>
2020-03-14 11:31:36 +0200

    Add datamap

---
+++ datamap.py
@@ -0,0 +1,47 @@
+"""
+DATA MAP
+"""
+from pprint import pprint
+import codecs
+
+fileName = './Data/datamap.csv'
+csvDelimiter = ';'
+dataMap = {}
+
+def readDataMapFile():
+    with codecs.open(fileName, encoding='utf-8') as f_in:
+        print("Luetaan tiedosto: " +fileName)
+        lines = f_in.readlines()
+
+        lineNbr = 0
+
+        for line in lines:
+            (table, column, title, datatype, area, db, offset, size) = line.split(csvDelimiter)
+
+            lineNbr += 1
+
+            if lineNbr < 2:
+                continue
+
+            print(line)
+
+            if not table in dataMap:
+                dataMap[table] = {}
+            elif not column in dataMap[table]:
+                dataMap[table][column] = {
+                    'title' : title,
+                    'datatype' : datatype,
+                    'area' : area,
+                    'db' : db,
+                    'offset' : offset,
+                    'size' : size,
+                }
+            else:
+                print("Duplicate entry in datamap csv, line: " + lineNbr + " +column")
+            
+
+if __name__ == "__main__":
+    readDataMapFile()
+    pprint(dataMap)
+
+
